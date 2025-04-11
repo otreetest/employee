6e0e9aa9f45a21cf95d9cfde1620b85be2796318
Meckimmmmm <63998902+Meckimmmmm@users.noreply.github.com>
2025-04-11 17:33:45 +0200

    settings changed

--- settings.py
+++ settings.py
@@ -8,6 +8,9 @@ SESSION_CONFIGS = [
     )
 ]
 
+DEBUG = False
+PRODUCTION = True
+
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
